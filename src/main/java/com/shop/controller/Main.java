6e2612ad37b8a493853668c57fb217d90b301dd5
vnpk4 <phucng165@gmail.com>
2026-07-04 19:23:29 +0700

    initial commit

--- src/main/java/com/shop/controller/Main.java
+++ src/main/java/com/shop/controller/Main.java
@@ -6,12 +6,6 @@ import java.util.*;
 
 void main() {
     ProductRepository repo = new ProductRepository();
-//    List<Product> danhSach = new ArrayList<>(
-//            List.of(
-//                    new Product(1,"Iphone X",100000),
-//                    new Product(2,"MSI",200000),
-//                    new Product(3,"Gaming", 300000)
-//            ));
     Cart cart = new Cart();
     cart.addToCart(1,2);
     cart.addToCart(2,4);
